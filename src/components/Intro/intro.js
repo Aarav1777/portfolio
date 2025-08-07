ea3766ee68f942d60708c8445395ba46e0df143a
Aarav kumar <aaravku7654@gmail.com>
2025-08-07 18:47:39 +0530

    add profile_pic

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assests/myImageAA.png';
-// import bg from '../../assests/myImageAA.png';
-
+import profilePic from '../../assests/profile_pic.jpg'; // ✅ Make sure path is correct
 
 function Intro() {
   return (
     <section id="intro">
-      <div className="introContent">
-        <span className="hello">Hello,</span>
-        <span className="introText">
-          I'm <span className="intoname">Aarav</span> <br /> a Web Developer & Programmer
-        </span>
-        <p className="intropara">
-          I am a B.Tech Computer Science student passionate about building websites <br />
-          and solving problems through code. <br />
-          I explore DSA, work on real-world projects, and continuously learn new technologies.
-        </p>
+      <div className="intro-row">
+        <div className="intro-left">
+          <span className="hello">Hello,</span>
+          <br></br>
+          <span className="introText">
+            I'm <span className="intoname">Aarav</span> <br /> a Web Developer & Programmer
+          </span>
+          <p className="intropara">
+            I am a B.Tech Computer Science student passionate about building websites <br />
+            and solving problems through code. <br />
+            I explore DSA, work on real-world projects, and continuously learn new technologies.
+          </p>
+          <br />
+          <br />
+          <br />
+          <br />
+          
+          <div className="buttons-container">
+            <a href="/Aarav A2 Ressume.pdf" download="Aarav_A2_Resume.pdf">
+              <button className="btn">Download CV</button>
+            </a>
+          </div>
+        </div>
 
-        {/* Buttons container for horizontal alignment */}
-        <div className="buttons-container">
-          {/* Download CV Button */}
-          {/* <a href="/Aarav A2 Ressume.pdf" download="Aa.pdf">
-           */}
-           <a href="/Aarav A2 Ressume.pdf" download="Aarav_A2_Resume.pdf">
-            <button className="btn">
-              Download CV
-            </button>
-          </a>
+        <div className="intro-right">
+          <img src={profilePic} alt="profile" className="intro-img" />
         </div>
       </div>
-      {/* <img src={bg} alt="profile" className="bg" />
-       */}
-       <img src={bg} alt="profile-pic" className="bg" />
-
-      
-       {/* <div class="bg" style="background-image: url('your-image.png');"></div> */}
-
     </section>
   );
 }
